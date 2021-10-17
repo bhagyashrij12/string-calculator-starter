74f363245d9816f1489df1dc58210a76a29059b8
Bhagyashri Jadhav <bhagyashridjadhav72@gmail.com>
2021-10-17 21:39:20 +0530

    perform addition operation

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        int i=Integer.parseInt(input);
+        
+        
+        return i + add(String input);
     }
 
 }
