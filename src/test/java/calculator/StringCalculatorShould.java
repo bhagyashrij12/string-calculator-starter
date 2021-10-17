187ccf40d831013a37100ff11ec21ba6fea6c205
Bhagyashri Jadhav <bhagyashridjadhav72@gmail.com>
2021-10-17 22:15:44 +0530

    evaluation of test cases

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,69 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+     //Test case to check string returns 0 on empty string
+       @Test
+        public void TestAddWithEmptyStringReturnsZero()
+        {
+            string numbers = string.Empty;
+            int result = stringCalculator.Add(numbers);
+            Assert.AreEqual(0,
+                            result);
+        }
+   //test case to check String returns addition of only one number present in string
+        @Test
+        public void TestAddWithSingleNumberInStringReturnsTheNumber()
+        {
+            string numbers = "1";
+            int result = stringCalculator.Add(numbers);
+            Assert.AreEqual(1,
+                            result);
+        }
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+    //Test case to check String returns addition of two numbers 
+       @TestCase("1,2,3,4,5,6,7,8,9,10")
+        public void TestAddWithMultipleNumbersInStringReturnsTheTotal(string inputNumbers)
+        {
+            int result = stringCalculator.Add(inputNumbers);
+            Assert.AreEqual(55,
+                            result);
+        }
+    //Test case to check that add method returns total if the string contains new lines and coma seperated values
+       @TestCase("1,2,3,4,5\n6,7,8\n9,10")
+        public void TestAddWithMultipleNumbersInStringSplitWithNewLinesAndCommasReturnsTheTotal(string inputNumbers)
+        {
+            int result = stringCalculator.Add(inputNumbers);
+            Assert.AreEqual(55,
+                            result);
+        }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+        @TestCase("1,2,3,4,\n6,7,\n9,10")
+        public void TestAddWithMultipleNumbersInStringReturnZeroForEmptyNumbers(string inputNumbers)
+        {
+            int result = stringCalculator.Add(inputNumbers);
+            Assert.AreEqual(42,
+                            result);
+        }
+     //Test to check using different delimeter
+        @TestCase("//;\n1;2")
+        public void TestAddWithMultipleNumbersInStringUsingDifferentDelimeter(string inputNumbers)
+        {
+            int result = stringCalculator.Add(inputNumbers);
+            Assert.AreEqual(3,
+                            result);
+        }
+     //test case to check method returns exception on negative numbers
+        @TestCase("//;\n1;-2")
+        [ExpectedException(typeof (ArgumentException))]
+        public void TestAddWithNegativeNumbersWillThrowException(string inputNumbers)
+        {
+            int result = stringCalculator.Add(inputNumbers);
+            Assert.AreEqual(3,
+                            result);
+        }
+    //test case to check if no is greater that 1000 then number is ignored
+       @Test
+       public final void whenOneOrMoreNumbersAreGreaterThan1000IsUsedThenItIsNotIncludedInSum() {
+        Assert.assertEquals(3+1000+6, StringCalculator8.add("3,1000,1001,6,1234"));
+}
 }
